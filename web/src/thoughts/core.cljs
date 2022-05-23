542eeec3e510bfac78fcc23210930ca125591d10
Andrey Krainyak <mode.andrew@gmail.com>
2022-05-24 00:14:04 +0800

    Implement sign-in form with in cljs

--- web/src/thoughts/core.cljs
+++ web/src/thoughts/core.cljs
@@ -1,7 +1,21 @@
 (ns thoughts.core
   (:require [reagent.dom :as rdom]))
 
-(defn hello-world []
-  [:div "Hello, World!"])
+(defn signin-form []
+  (let [row (fn [x] [:div {:class "row"}
+                      [:div {:class "col"}
+                        x]])]
+    [:div (row [:input {:id "email"
+                        :type "email"
+                        :class "form-control my-1"
+                        :placeholder "E-mail"}])
+          (row [:input {:id "password"
+                        :type "password"
+                        :class "form-control my-1"
+                        :placeholder "Password"}])
+          (row [:button {:type "submit"
+                         :style {:width "100%"}
+                         :class "btn btn-primary my-1"}
+                         "Sign in"])]))
 
-(rdom/render [hello-world] (js/document.getElementById "app"))
+(rdom/render [signin-form] (js/document.getElementById "app"))
