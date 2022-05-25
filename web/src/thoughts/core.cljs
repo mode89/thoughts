198b36b87ec49e9f2a36583b1fe1a88855f2f7c8
Andrey Krainyak <mode.andrew@gmail.com>
2022-05-25 22:24:27 +0800

    Sign-up form in cljs

--- web/src/thoughts/core.cljs
+++ web/src/thoughts/core.cljs
@@ -18,4 +18,25 @@
                          :class "btn btn-primary my-1"}
                          "Sign in"])]))
 
+(defn signup-form []
+  (let [row (fn [x] [:div {:class "row"}
+                      [:div {:class "col"}
+                        x]])]
+    [:div (row [:input {:id "email"
+                        :type "email"
+                        :class "form-control my-1"
+                        :placeholder "E-mail"}])
+          (row [:input {:id "password"
+                        :type "password"
+                        :class "form-control my-1"
+                        :placeholder "Password"}])
+          (row [:input {:id "repeat-password"
+                        :type "password"
+                        :class "form-control my-1"
+                        :placeholder "Repeat password"}])
+          (row [:button {:type "submit"
+                         :style {:width "100%"}
+                         :class "btn btn-primary my-1"}
+                         "Sign in"])]))
+
 (rdom/render [signin-form] (js/document.getElementById "app"))
