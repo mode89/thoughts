62a1c9edc96f970f4d737dc020089632ff2510e4
Andrey Krainyak <mode.andrew@gmail.com>
2022-05-29 10:28:21 +0800

    Authenticate web client

--- web/project.clj
+++ web/project.clj
@@ -1,5 +1,7 @@
 (defproject thoughts "0.1.0"
   :dependencies [
+    [cljs-http "0.1.46"]
+    [org.clojure/core.async "1.5.648"]
     [reagent "1.1.1"]]
   :profiles {:dev {:dependencies [
     [com.bhauman/figwheel-main "0.2.18"]]}}
--- web/src/thoughts/core.cljs
+++ web/src/thoughts/core.cljs
@@ -1,20 +1,37 @@
 (ns thoughts.core
-  (:require [reagent.dom :as rdom]))
+  (:require [cljs-http.client :as http]
+            [clojure.core.async :refer [chan close! go go-loop put! <!]]
+            [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+(def BACKEND-URL "http://192.168.0.101:5000")
+
+(def credentials (chan))
 
 (defn signin-form []
   (let [row (fn [x] [:div {:class "row"}
                       [:div {:class "col"}
-                        x]])]
-    [:form {:id "signin"}
+                        x]])
+        state (r/atom {})
+        on-change #(swap! state assoc
+                          (-> % .-target .-id keyword)
+                          (-> % .-target .-value))]
+    [:form {:id "signin"
+            :on-submit
+              (fn [e] (do
+                (.preventDefault e)
+                (put! credentials @state)))}
       [:div {:id "center" :class "container"}
         (row [:input {:id "email"
                       :type "email"
                       :class "form-control my-1"
-                      :placeholder "E-mail"}])
+                      :placeholder "E-mail"
+                      :on-change on-change}])
         (row [:input {:id "password"
                       :type "password"
                       :class "form-control my-1"
-                      :placeholder "Password"}])
+                      :placeholder "Password"
+                      :on-change on-change}])
         (row [:button {:type "submit"
                        :style {:width "100%"}
                        :class "btn btn-primary my-1"}
@@ -53,4 +70,21 @@
 (defn random-thought [text]
   [:div {:id "random-thought"} text])
 
-(rdom/render [signin-form] (js/document.getElementById "app"))
+(defonce state (r/atom {:content (signin-form)}))
+
+(defn content []
+  (:content @state))
+
+(rdom/render [content] (js/document.getElementById "app"))
+
+(defn authenticate [credentials]
+  (http/post (str BACKEND-URL "/auth") {:form-params credentials}))
+
+(def go-authentication
+  (go-loop []
+    (let [cred (<! credentials)
+          response (<! (authenticate (select-keys cred [:email :password])))]
+      (if (:success response)
+        (swap! state assoc :token (get-in response [:body :token]))
+        (js/alert "Failed to authenticate."))
+      (recur))))
